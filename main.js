19258861cc9675959ce2cdc23980198978606fdb
dranixv12 <60946978+dranixv12@users.noreply.github.com>
2020-03-12 15:12:01 -0600

    Didnt
    worked on a few thibngs

---
+++ main.js
@@ -0,0 +1,62 @@
+//Change the "android vs apple" to images
+
+document.getElementById('img').src= 'images/Android-Logo.jpg';
+
+
+
+//Change the 'Explore the Debate' link so that the link says 'Android Home
+function{
+
+
+
+}
+//Change the 'Explore the Debate' link so that the background color is #a4c93b
+
+
+
+
+//hange the 'Explore the Debate' link
+
+
+
+
+//Change the background color of the body to 
+
+
+
+
+
+
+
+
+
+//APPLE 
+//If the user clicks the Apple button
+
+
+
+
+//Change the 'Android vs Apple' image to the Apple Logo in the images folder
+
+
+
+
+
+//Change the 'Explore the Debate' link so that the link says 'Apple Home'
+
+
+
+
+//Change the 'Explore the Debate' link so that the background color is #b6bcca
+
+
+
+
+
+//Change the 'Explore the Debate' link so that the href is now
+
+
+
+//Change the background color of the body to #b6bcca
+
+
